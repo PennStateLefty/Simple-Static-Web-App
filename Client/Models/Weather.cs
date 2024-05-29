4179e615ddf68c5221916b41a3f052f759ce7df3
Jeremiah Gutherie <37122175+PennStateLefty@users.noreply.github.com>
2024-05-29 16:17:05 -0400

    Removed hard coding

--- Client/Models/Weather.cs
+++ Client/Models/Weather.cs
@@ -1,8 +1,25 @@
+using System.Text.Json.Serialization;
+
 public class Weather
 {
+    [JsonPropertyName("temperature")]
     public double Temperature { get; set; }
+
+    [JsonPropertyName("location")]
     public string? LocationName { get; set; }
+
+    [JsonPropertyName("wind_speed")]
     public double WindSpeed { get; set; }
-    public string? WindDirection { get; set; }
+
+    [JsonPropertyName("wind_direction")]
+    public int WindDirection { get; set; }
+
+    [JsonPropertyName("conditions")]
     public string? CurrentConditions { get; set; }
+
+    public static Weather FromJson(string json)
+    {
+        var weather = System.Text.Json.JsonSerializer.Deserialize<Weather>(json);
+        return weather;
+    }
 }
